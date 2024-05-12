1ff0c7bdd6a739fa6143fc783f845a96c12bf85c
tyr0v-k <tyr0v.k@yandex.ru>
2024-05-12 20:09:26 +0300

    main, Main.java: print "Финальный коммит в ветке main"

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
+        System.out.print("Финальный коммит в ветке main");
     }
 }
