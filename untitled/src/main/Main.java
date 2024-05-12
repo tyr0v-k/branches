5ec1dbcfb3cf5b132d1e2520a3c0d60209c4ced5
tyr0v-k <tyr0v.k@yandex.ru>
2024-05-12 19:59:15 +0300

    dev, Main.java: print "dev, 3"

--- untitled/src/main/Main.java
+++ untitled/src/main/Main.java
@@ -2,5 +2,6 @@ public class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
         System.out.print("dev, 2");
+        System.out.print("dev, 3");
     }
 }
